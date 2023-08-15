e46864145efb4c123502b299c927f1ee0d639197
Natasha Chiu <chiuchiu07@gmail.com>
2023-08-16 05:06:45 +0800

    styling

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -5,7 +5,7 @@ export default class Panel extends Component {
     const { label, value, onSelect } = this.props;
 
     return (
-      <section className="dashboard_panel" onClick={onSelect} >
+      <section className="dashboard__panel" onClick={onSelect} >
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
       </section>
